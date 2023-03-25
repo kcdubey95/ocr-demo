a2322fdd6962f769d94246bad110912d27e3a4a9
kcdubey95 <dubey.krishnachand@gmail.com>
2023-03-25 13:48:08 +0530

    ocr code update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import easyocr
 # git hub
 # https://github.com/JaidedAI/EasyOCR
-image=('ocr.jpg')
+image = 'ocr.jpg'
 reader = easyocr.Reader(['en'])
 result = reader.readtext(image,  detail=0, paragraph=True)
 print(result)
